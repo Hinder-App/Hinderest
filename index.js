6c4aaa15474062ac4f50d71b8b326aa615166626
Miguel Uy <yuleugim@gmail.com>
2017-03-02 23:41:41 -0500

    Add dotenv and implement basic server

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-console.log('Hello World')
+require('dotenv').load()
+const restify = require('restify')
+const MongoClient = require('mongodb').MongoClient
 
-console.log(process.env.MONGODB_URI)
+var db
+
+MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  db = database
+
+  console.log(`Database ${db.databaseName} connected`)
+
+  const server = restify.createServer()
+  server.get('/hello/:name', respond)
+  server.head('/hello/:name', respond)
+
+  server.listen(process.env.PORT, () => {
+    console.log(`${server.name} listening at ${server.url}`)
+  })
+})
+
+function respond(req, res, next) {
+  res.send(`Hello ${req.params.name}`)
+  next()
+}
